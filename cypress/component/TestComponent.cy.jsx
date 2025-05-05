458d1a2cca4f13b9842b7d688968c44ee65de5ce
unknown <ernestoaleman00@gmail.com>
2025-05-05 01:13:43 -0500

    Hopefully fixed a silly mistake. Mistmatch between client side test component & testcomponent test at cypress

--- cypress/component/TestComponent.cy.jsx
+++ cypress/component/TestComponent.cy.jsx
@@ -4,6 +4,6 @@ import TestComponent from '../../client/src/components/TestComponent';
 describe('TestComponent', () => {
   it('renders correctly', () => {
     cy.mount(<TestComponent />);
-    cy.contains('Hello from TestComponent');
+    cy.contains('TestComponent To Rule Them All');
   });
 });
